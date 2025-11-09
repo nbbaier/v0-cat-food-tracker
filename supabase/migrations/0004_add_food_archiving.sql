6bc4ed983752fc8effd2d1eace001a232dcba868
Nico Baier <nico.baier@gmail.com>
2025-11-08 23:20:03 -0600

    feat(supabase/migrations): add and update food and meal migrations

--- supabase/migrations/0004_add_food_archiving.sql
+++ supabase/migrations/0004_add_food_archiving.sql
@@ -1,6 +1,12 @@
 -- Add archived column to foods table
-ALTER TABLE "foods" ADD COLUMN "archived" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE "foods"
+ADD COLUMN "archived" boolean DEFAULT false NOT NULL;
 
+--> statement-breakpoint
 -- Update foreign key constraint to use restrict on delete
-ALTER TABLE "meals" DROP CONSTRAINT "meals_foodId_foods_id_fk";--> statement-breakpoint
-ALTER TABLE "meals" ADD CONSTRAINT "meals_foodId_foods_id_fk" FOREIGN KEY ("foodId") REFERENCES "public"."foods"("id") ON DELETE restrict ON UPDATE no action;
+ALTER TABLE "meals"
+DROP CONSTRAINT "meals_foodId_foods_id_fk";
+
+--> statement-breakpoint
+ALTER TABLE "meals"
+ADD CONSTRAINT "meals_foodId_foods_id_fk" FOREIGN KEY ("foodId") REFERENCES "public"."foods" ("id") ON DELETE restrict ON UPDATE no action;
