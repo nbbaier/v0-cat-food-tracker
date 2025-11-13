dfaca2c3b2cf373bd7bc45a5c9ffc3ee2254130a
Nico Baier <nico.baier@gmail.com>
2025-11-13 13:34:09 -0600

    bug: fixed build error

--- app/foods/page.tsx
+++ app/foods/page.tsx
@@ -1,6 +1,6 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { FoodsPageClient } from "@/components/foods-page-client";
+import { FoodsPageClient } from "@/components/foods/foods-page-client";
 import { auth } from "@/lib/auth";
 
 export default async function FoodsPage() {
